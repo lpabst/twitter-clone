aa82c51fb1a641d985a187d4a6e610d45aea03f9
loren pabst <loren pabst>
2017-04-21 15:17:08 -0600

    content box is working entirely now

--- js/animation.js
+++ js/animation.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 var compose_box = $('.tweet-compose');
 var charCount = $('#char-count');
+var submit_btn = $('#tweet-submit');
 
 //When the user clicks on the textarea, the textarea should 
 //double in size and the character count and Tweet buttons 
@@ -12,26 +13,41 @@ compose_box.click(function(){
 });
 
 //As the user types, the character count should decrease.
+
 //When there are 10 or less characters, the character 
 //counter should turn red.
-compose_box.keypress(function(e){
-    var count = charCount.html();
 
-    if (e.which === 8){
-        count++;
-    }else{
-        count--;
+//If the user puts in more than 140 characters, the 
+//tweet button should be disabled (and re-enabled when 
+//there are <= 140 chars).
+compose_box.keyup(function(e){
+    var text = compose_box.val();
+    var arr = text.split('');
+    var count = arr.length;
+
+    if (count > 140){
+        submit_btn.removeClass("button");
+        submit_btn.addClass("button:disabled");
+        //submit_btn.css('display', 'none');
+    }else if(count <= 140){
+        submit_btn.removeClass("button:disabled");
+        submit_btn.addClass("button");
     }
     
-    charCount.html(count);
+    charCount.html(140 - count);
 
     if (charCount.html() <= 10){
         charCount.css('color', 'red');
+    }else{
+        charCount.css('color', '#999');
     }
 
 });
 
-
+submit_btn.click(function(){
+    var newTweet  = $('.tweet').clone();
+    $('#stream').prepend(newTweet);
+});
 
 
 
